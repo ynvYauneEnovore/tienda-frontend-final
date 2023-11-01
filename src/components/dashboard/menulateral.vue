1b8d744e2cc3fb3647d762e90e997df6b0cdd75f
ynvYauneEnovore <yovanuxf@gmail.com>
2023-11-01 17:58:18 -0400

    add menu lateral efectos landing

--- src/components/dashboard/menulateral.vue
+++ src/components/dashboard/menulateral.vue
@@ -1,57 +1,151 @@
-<script>
-export default {
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
   data() {
     return {
-      showSearchBox: false,
-      searchText: "",
+      searchBoxVisible: true,
+      ButtonBoxVisible: false
     };
   },
   methods: {
-    openSearchBox() {
-      this.showSearchBox = true;
+    mostrarBusqueda() {
+      this.searchBoxVisible = true;
+      this.ButtonBoxVisible = false;
     },
-    closeSearchBox() {
-      this.showSearchBox = false;
-      this.searchText = "";
+    ocultarBusqueda() {
+      this.searchBoxVisible = false;
+      this.ButtonBoxVisible = true;
     },
+    mretr() {
+      setTimeout(() => {
+        this.searchBoxVisible = false;
+        this.ButtonBoxVisible = true;
+      }, 3000);
+    }
   },
-};
+  mounted() {
+    this.mretr();
+  }
+});
 </script>
 
-<template>
-   <div class="boton-apertura" @click="openSearchBox">
-      <lord-icon
-    src="https://cdn.lordicon.com/swcqkzdc.json"
-    trigger="hover"
-    style="width:150px;height:150px">
-</lord-icon>
-   </div>
 
-   <div class="floating-search-box">
-     <div class="header">
-       <h2>Búsqueda</h2>
-       <button class="custom-close-button" @click="showSearchBox">Cerrar</button>
-     </div>
-     <div class="main">
-       <input type="text" placeholder="Buscar..." v-model="searchText">
-     </div>
-     <div class="footer">
-       <!-- Aquí puedes agregar contenido adicional para el footer si lo deseas -->
-     </div>
-   </div>
- </template>
+<template>
+  <div class="boutososo" :style="{ opacity: ButtonBoxVisible ? 1 : 0 }">
+<button @click="mostrarBusqueda">
+  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-layout-sidebar-right-collapse" width="60" height="60" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+  <path d="M15 4v16" />
+  <path d="M9 10l2 2l-2 2" />
+</svg>
+</button>
+</div>
+
+
+
+  <div class="floating-search-box" :style="{ opacity: searchBoxVisible ? 1 : 0 }">
+    <div class="header">
+      <h2>Búsqueda</h2>
+      <button class="custom-close-button" @click="ocultarBusqueda">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-square-rounded-x-filled" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M12 2l.324 .001l.318 .004l.616 .017l.299 .013l.579 .034l.553 .046c4.785 .464 6.732 2.411 7.196 7.196l.046 .553l.034 .579c.005 .098 .01 .198 .013 .299l.017 .616l.005 .642l-.005 .642l-.017 .616l-.013 .299l-.034 .579l-.046 .553c-.464 4.785 -2.411 6.732 -7.196 7.196l-.553 .046l-.579 .034c-.098 .005 -.198 .01 -.299 .013l-.616 .017l-.642 .005l-.642 -.005l-.616 -.017l-.299 -.013l-.579 -.034l-.553 -.046c-4.785 -.464 -6.732 -2.411 -7.196 -7.196l-.046 -.553l-.034 -.579a28.058 28.058 0 0 1 -.013 -.299l-.017 -.616c-.003 -.21 -.005 -.424 -.005 -.642l.001 -.324l.004 -.318l.017 -.616l.013 -.299l.034 -.579l.046 -.553c.464 -4.785 2.411 -6.732 7.196 -7.196l.553 -.046l.579 -.034c.098 -.005 .198 -.01 .299 -.013l.616 -.017c.21 -.003 .424 -.005 .642 -.005zm-1.489 7.14a1 1 0 0 0 -1.218 1.567l1.292 1.293l-1.292 1.293l-.083 .094a1 1 0 0 0 1.497 1.32l1.293 -1.292l1.293 1.292l.094 .083a1 1 0 0 0 1.32 -1.497l-1.292 -1.293l1.292 -1.293l.083 -.094a1 1 0 0 0 -1.497 -1.32l-1.293 1.292l-1.293 -1.292l-.094 -.083z" fill="currentColor" stroke-width="0" />
+</svg>
+      </button>
+    </div>
+    <div class="main">
+
+
+      <h3 class="mb-3 mt-1 font-semibold text-gray-900 dark:text-white">Technology</h3>
+
+<form>
+    <div class="flex">
+        <label for="search-dropdown" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
+        <button id="dropdown-button" data-dropdown-toggle="dropdown" class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 dark:border-gray-700 dark:text-white rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800" type="button">All categories <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+  </svg></button>
+        <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
+            <li>
+                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Shopping</a>
+            </li>
+            <li>
+                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Images</a>
+            </li>
+            <li>
+                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">News</a>
+            </li>
+            <li>
+                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Finance</a>
+            </li>
+            </ul>
+        </div>
+        <div class="relative w-full">
+            <input type="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-100 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search" required>
+            <button type="submit" class="absolute top-0 right-0 p-2.5 h-full text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+  </svg></button>
+        </div>
+    </div>
+</form>
+
+
+<h3 class="mt-3 font-semibold text-gray-900 dark:text-white">Technology</h3>
+<ul class="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+    <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+        <div class="flex items-center pl-3">
+            <input id="vue-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+            <label for="vue-checkbox" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Electrónica</label>
+        </div>
+    </li>
+    <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+        <div class="flex items-center pl-3">
+            <input id="react-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+            <label for="react-checkbox" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Moda</label>
+        </div>
+    </li>
+    <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+        <div class="flex items-center pl-3">
+            <input id="angular-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+            <label for="angular-checkbox" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Hogar </label>
+        </div>
+    </li>
+    <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+        <div class="flex items-center pl-3">
+            <input id="laravel-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+            <label for="laravel-checkbox" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Entretenimiento</label>
+        </div>
+    </li>
+</ul>
+
+
+<label for="price-range" class="block mt-3 text-sm font-medium text-gray-900 dark:text-white">Price Range</label>
+<input id="price-range" type="range" min="0" max="100" value="50" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700 mb-3">
+
+
+
+    </div>
+    <div class="footer">
+      <!-- Aquí puedes agregar contenido adicional para el footer si lo deseas -->
+    </div>
+  </div>
+</template>
+
  
  <style scoped>
  .floating-search-box {
-   
+
    position: fixed;
-   top: 30%;
+   top: 15%;
    right: 0%;
-   width: 300px;
-   background-color: #fff;
+   width: 350px;
+   background-color: #ffffff;
    border: 1px solid #ccc;
    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-   z-index: 999;
+   z-index: 1;
+   transition: opacity 1.0s;
  }
 
  .boton-apertura {
@@ -61,8 +155,7 @@ export default {
    right: 0%;
    background-color: #fff;
    border: 1px solid #ccc;
-   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-   z-index: 999;
+
  }
  
  .header {
@@ -83,27 +176,25 @@ export default {
    padding: 10px;
  }
  
- input {
-   width: 100%;
-   padding: 5px;
-   margin-bottom: 10px;
- }
+
  
- .footer {
-   /* Estilos del footer, puedes personalizarlo según tus necesidades */
- }
- 
- button.custom-close-button {
-   background-color: #007bff;
-   color: #fff;
-   border: 1px solid #007bff;
-   border-radius: 0 0 5px 0; /* Borde redondeado solo en el lado izquierdo (parte inferior del header) */
-   padding: 5px 10px;
-   cursor: pointer;
- }
- 
- button.custom-close-button:hover {
-   background-color: #0056b3;
+ .boutososo {
+  position: fixed;
+  top: 40%;
+  right: 0%;
+  background-color: #007bff; /* Cambia el color de fondo a uno oscuro */
+  color: #00487f; /* Cambia el color del texto a blanco */
+  font-size: 18px; /* Aumenta el tamaño del texto */
+  border-radius: 10px; /* Agrega bordes redondeados */
+  z-index: 999; /* Asegura que el elemento esté en la parte superior */
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3); /* Agrega sombra al elemento */
+  transition: right 0.3s ease; /* Agrega una transición suave al desplazarlo */
+}
+
+
+ .boutososo :hover{
+  background-color: rgba(16, 223, 168, 0.11);
+
  }
  </style>
  
